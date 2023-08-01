b7e5577533993f713ebbd67cd3070d6162207fdf
btcs-datpt <datpt@btc-studio.io>
2023-08-02 00:59:43 +0700

    config env

--- main.go
+++ main.go
@@ -7,16 +7,23 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/tuda4/simple_bank/api"
 	db "github.com/tuda4/simple_bank/db/sqlc"
+	"github.com/tuda4/simple_bank/util"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:123456@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:123456@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	connect, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot load config::::", err)
+	}
+
+	connect, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to database", err)
@@ -25,7 +32,7 @@ func main() {
 	store := db.NewStore(connect)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
